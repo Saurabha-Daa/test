4f0fb970978da263232fc6126815148566e51608
Saurabha-Daa <99875345+Saurabha-Daa@users.noreply.github.com>
2022-02-23 13:53:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,8 +115,8 @@ def inference(query):
 def main():
     st.sidebar.write("This predictor is based on a Kaggle competition. This competition and datasets can be accessed from https://www.kaggle.com/c/home-credit-default-risk/overview. The source code for this predictor can be accessed from https://github.com/Saurabha-Daa/test.")
     st.write('LOAN DEFAULT TENDENCY PREDICTOR')
-    template = convert_df(query_template)
-    st.download_button("Download template for query data", query_template, "query_template.csv", key='text/csv')
+    template = query_template.df.to_csv().encode('utf-8')
+    st.download_button("Download template for query data", template, "query_template.csv", key='text/csv')
     uploaded_file = st.file_uploader("Choose a query data file")       
     if uploaded_file is not None:
         query = dataframe_optimizer(pd.read_csv(uploaded_file))
