acc0aea0c8334d47dc912f3a2bc42c5033acba3b
Saurabha-Daa <99875345+Saurabha-Daa@users.noreply.github.com>
2022-02-24 16:57:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def main():
             query_data_with_prediction = query_data_with_prediction.to_csv().encode('utf-8')
             st.download_button("Download query data with predictions as CSV", query_data_with_prediction, "prediction.csv", key='text/csv')
         else:
-          print("Query columns do not match the columns of required format as given in template. Please upload query data in the given format.")
+            st.write("Query columns do not match the columns of required format as given in template. Please upload query data in the given format.")
 
 if __name__=='__main__':
     main()
